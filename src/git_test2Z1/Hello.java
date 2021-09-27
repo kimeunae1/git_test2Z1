f021b221424a26c59efa3a4172dc0f6d98c9e0bb
Eunae Kim <rkdmfdksqn@naver.com>
2021-09-27 22:16:15 +0900

    화면 출력 방법 수정

--- src/git_test2Z1/Hello.java
+++ src/git_test2Z1/Hello.java
@@ -1,17 +1,12 @@
 package git_test2Z1;
 
 public class Hello {
-	String message;
+	
+	String message="안녕하세요";
 
-	public Hello() {
-		this("안녕하세요");
-	}
+	
 
-	public Hello(String message) {
-		this.message = message;
-	}
-
-	public void say() {
-		System.out.println(message);
+	public String say() {
+		return message;
 	}
 }
--- src/git_test2Z1/Main.java
+++ src/git_test2Z1/Main.java
@@ -2,7 +2,7 @@ package git_test2Z1;
 
 public class Main {
 	public static void main(String[] args) {
-		Hello h = new Hello("hello world");
-		h.say();
+		Hello hello = new Hello();
+		System.out.println(hello.say())
 	}
 }
